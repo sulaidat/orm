1a5c035a827287534f9c163f80b67a165ce7c939
=Minnas <=phannhattrieu01234@gmail.com>
2024-01-12 01:39:22 +0700

    separate IqueryTarget into small detail crud action

---
+++ src/main/java/org/jminiorm/crud/IInsertQuery.java
@@ -0,0 +1,44 @@
+package org.jminiorm.crud;
+
+import org.jminiorm.exception.DBException;
+import org.jminiorm.query.generic.IGenericInsertQuery;
+import org.jminiorm.query.orm.IORMInsertQuery;
+
+import java.util.Collection;
+
+public interface IInsertQuery {
+    /**
+     * Begins a JPA insert query.
+     *
+     * @param clazz
+     * @param <T>
+     * @return
+     * @throws DBException
+     */
+    <T> IORMInsertQuery<T> insert(Class<T> clazz);
+
+    /**
+     * Insert an instance of JPA annotated class into its table. Generated id is set if any.
+     *
+     * @param obj
+     * @throws DBException
+     */
+    <T> void insert(T obj) throws DBException;
+
+    /**
+     * Insert instances of a JPA annotated class into their table. Generated ids are set if any.
+     *
+     * @param objs
+     * @throws DBException
+     */
+    <T> void insert(Collection<T> objs) throws DBException;
+
+    /**
+     * Begins a generic insert query into the given table.
+     *
+     * @param table
+     * @return
+     * @throws DBException
+     */
+    IGenericInsertQuery insert(String table) throws DBException;
+}
